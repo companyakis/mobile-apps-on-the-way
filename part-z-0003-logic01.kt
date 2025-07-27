54810f62f647c25388d655aa0141aa52a05d3feb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-28 00:10:53 +0300

    Update part-z-0003-logic01.kt

--- part-z-0003-logic01.kt
+++ part-z-0003-logic01.kt
@@ -6,7 +6,7 @@ fun main() {
     
     var cond3: Boolean = 5 > 4
     
-  	var cond4: Boolean = 4 >= 10
+    var cond4: Boolean = 4 >= 10
     
     print("$cond1 - $cond2 - $cond3 - $cond4") // true - true - true - false
      
