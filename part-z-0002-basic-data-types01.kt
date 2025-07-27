da26d84309042b680ca5ba3e9b279bf1abb23cdb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-27 23:56:21 +0300

    Create part-z-0002-basic-data-types01.kt

---
+++ part-z-0002-basic-data-types01.kt
@@ -0,0 +1,21 @@
+fun main() {
+    
+    var age: Byte = 99 
+    
+    var year: Short = 2025 
+    
+    var sales: Int = 3_250_421 
+    
+    var gdp: Long = 870_954_612_500
+    
+    println(age)
+    println(year)
+    println(sales)
+    println(gdp)
+    
+}
+
+// 99
+// 2025
+// 3250421
+// 870954612500
