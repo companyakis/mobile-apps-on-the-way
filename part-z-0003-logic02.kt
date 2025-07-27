481241f8763890032cc993aa9997ebf9abbda426
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-28 00:13:42 +0300

    Create part-z-0003-logic02.kt

---
+++ part-z-0003-logic02.kt
@@ -0,0 +1,14 @@
+fun main() {
+    
+    var cond1: Boolean = (3 > 2) && (4 > -5)
+    
+    var cond2: Boolean = (11 != 10) && (7 >= 100)
+    
+    println(cond1)
+    
+    println(cond2)
+    
+}
+
+// true
+// false
