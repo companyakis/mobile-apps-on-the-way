a400e9489e498d6c219d1c45c4dd99f7036ac754
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-28 00:17:30 +0300

    Create part-z-0003-logic04.kt

---
+++ part-z-0003-logic04.kt
@@ -0,0 +1,19 @@
+fun main() {
+    
+    var cond1: Boolean = true && true
+    
+    var cond2: Boolean = true && false
+    
+    var cond3: Boolean = false && false
+    
+    println(cond1)
+    
+    println(cond2)
+    
+    println(cond3)
+       
+}
+
+// true
+// false
+// false
