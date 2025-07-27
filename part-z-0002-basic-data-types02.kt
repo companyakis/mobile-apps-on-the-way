a1805b117fb1ca64d8f55271f51c26dc630bfd4f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-28 00:00:23 +0300

    Create part-z-0002-basic-data-types02.kt

---
+++ part-z-0002-basic-data-types02.kt
@@ -0,0 +1,14 @@
+fun main() {
+    
+    var pi1: Double = 22.0 / 7.0 
+    
+    println(pi1)
+    
+    var pi2: Float = 22.0f / 7.0f 
+    
+    println(pi2)
+     
+}
+
+// 3.142857142857143
+// 3.142857
