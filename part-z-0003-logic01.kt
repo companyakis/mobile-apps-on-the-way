b64a630eec393c87ba47c0ccac789a1a4b9c1d50
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-28 00:10:22 +0300

    Create part-z-0003-logic01.kt

---
+++ part-z-0003-logic01.kt
@@ -0,0 +1,14 @@
+fun main() {
+    
+    var cond1: Boolean = 2 == 2 
+    
+    var cond2: Boolean = 3 != 4
+    
+    var cond3: Boolean = 5 > 4
+    
+  	var cond4: Boolean = 4 >= 10
+    
+    print("$cond1 - $cond2 - $cond3 - $cond4") // true - true - true - false
+     
+}
+
