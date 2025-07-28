7895db560f1017c15955d49cd55c648cbe84ccba
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-28 16:26:59 +0300

    Create part-z-0009-type-conversion2.kt

---
+++ part-z-0009-type-conversion2.kt
@@ -0,0 +1,16 @@
+fun main() {
+    
+    var x: Int = 100 
+    
+    var y: Double = 1.1111
+    
+    var z: Float = 3.1222f
+    
+    var str1 = x.toString()
+	
+    var str2 = y.toString()
+    
+    var str3 = z.toString()
+    
+}
+
