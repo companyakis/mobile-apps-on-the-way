71e76297849f42bb68e5dab46841b40c66ff45c1
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-28 15:48:36 +0300

    Create part-z-0006-scope1.kt

---
+++ part-z-0006-scope1.kt
@@ -0,0 +1,12 @@
+fun main() {
+    
+    var num1: Byte = -5
+    
+    {
+        var num2: Byte = 12
+    }
+    
+    println("Sum 1: ${num1 + num2}") // Error! => Unresolved reference 'num2'.
+
+}
+
