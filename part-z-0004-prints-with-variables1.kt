dfb810edc442ddfee2476f1cc0f6428d086459fc
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-28 15:35:40 +0300

    Create part-z-0004-prints-with-variables1.kt

---
+++ part-z-0004-prints-with-variables1.kt
@@ -0,0 +1,10 @@
+fun main() {
+    
+    var firstName = "Mustafa"
+    
+    var lastName = "Büyükdereli"
+    
+    println("My first-name is $firstName and last-name is $lastName!") // My first-name is Mustafa and last-name is Büyükdereli!
+    
+}
+
