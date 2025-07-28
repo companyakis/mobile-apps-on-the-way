311fd0c834991fb46be0f1a9c7a76b57c35f10ce
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-28 16:25:36 +0300

    Create part-z-0009-type-conversion1.kt

---
+++ part-z-0009-type-conversion1.kt
@@ -0,0 +1,16 @@
+fun main() {
+    
+    var x: Int = 100 
+    
+    var y: Double = 1.1111
+    
+    var z: Float = 3.1222f
+    
+    var num1 = x.toDouble()
+	
+    var num2 = y.toInt()
+    
+    var num3 = z.toInt()
+    
+}
+
