5a7d26fb61bec7f4588903d324419efc2d04d0a6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-28 15:39:40 +0300

    Create part-z-0004-prints-with-variables2.kt

---
+++ part-z-0004-prints-with-variables2.kt
@@ -0,0 +1,14 @@
+fun main() {
+    
+	var radius: Byte = 5 
+    
+    var pi: Double = 3.14
+    
+    println("Perimeter: ${2 * pi * radius}")
+    
+    println("Area: ${pi * radius * radius}")
+    
+}
+
+// Perimeter: 31.400000000000002
+// Area: 78.5
